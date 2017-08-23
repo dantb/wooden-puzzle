67c173454d3c4fcf575a57887c44f822b1216738
dantb <dantb@hotmail.co.uk>
2017-08-23 20:17:06 +0100

    Created outline of basic data model

--- WoodenPuzzle/WoodenPuzzle/Program.cs
+++ WoodenPuzzle/WoodenPuzzle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WoodenPuzzle
 {
@@ -7,6 +8,45 @@ namespace WoodenPuzzle
         static void Main(string[] args)
         {
             Console.ReadKey();
+        }
+    }
+
+    public class Block
+    {
+        public Block(List<BlockSlot> blockSlots)
+        {
+            BlockSlots = blockSlots;
+        }
+
+        public List<BlockSlot> BlockSlots { get;  }
+    }
+
+    public abstract class BlockSlot
+    {
+        public abstract bool CompatibleWith(BlockSlot other);
+    }
+
+    public class Joint : BlockSlot
+    {
+        public override bool CompatibleWith(BlockSlot other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Hole : BlockSlot
+    {
+        public override bool CompatibleWith(BlockSlot other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FlatSpace : BlockSlot
+    {
+        public override bool CompatibleWith(BlockSlot other)
+        {
+            throw new NotImplementedException();
         }
     }
 }
