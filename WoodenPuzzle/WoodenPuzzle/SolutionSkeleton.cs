7195e15066efe842c49cc7ea498266c0020d064b
dantb <dantb@hotmail.co.uk>
2017-08-23 22:58:30 +0100

    Previous commit added this file, will have layers for potential solutions. Not yet implemented

--- WoodenPuzzle/WoodenPuzzle/SolutionSkeleton.cs
+++ WoodenPuzzle/WoodenPuzzle/SolutionSkeleton.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WoodenPuzzle
+﻿namespace WoodenPuzzle
 {
     /// <summary>
     /// The skeleton of a solution, in the form of two layers - the top and bottom.
     /// </summary>
     public class SolutionSkeleton
     {
-
-
-
     }
 }
